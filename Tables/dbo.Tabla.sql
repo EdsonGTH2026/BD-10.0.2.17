4106e833b8a5a2285f466e51a9e9ab1cbe122f76
EdsonGTH2026 <esolorio02@gmail.com>
2026-03-24 16:36:55 -0600

    Source Control FinamioBuro

---
+++ Tables/dbo.Tabla.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Tabla] (
+  [C1] [char](100) NULL,
+  [C2] [char](10) NULL,
+  [C3] [char](10) NULL,
+  [C4] [char](10) NULL,
+  [C5] [char](10) NULL,
+  [C6] [char](10) NULL,
+  [C7] [char](10) NULL
+)
+ON [PRIMARY]
+GO
